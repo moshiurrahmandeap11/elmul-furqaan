651641114e36083244b0dcd9be5ec6f02c244a7c
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2025-10-04 17:28:07 +0600

    live deploy

--- src/hooks/axiosIntance/AxiosIntance.jsx
+++ src/hooks/axiosIntance/AxiosIntance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api", 
+  baseURL: "https://elmul-furqaan-server.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
